7cc0acffb3faca01ad688d0bd4bb27c2013b7867
BERKKAN <berkkanrencber@gmail.com>
2022-03-28 12:48:35 +0300

    //Skeleton

---
+++ src/com/company/SignedConverter.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class SignedConverter {
+    String byteOrder;
+    int sizeOfDataType;
+    String value;
+
+    void SignedConverter(String byteOrdering,int sizeOfDataType,String value){
+        this.byteOrder=byteOrdering;
+        this.sizeOfDataType=sizeOfDataType;
+        this.value=value;
+
+    }
+}
---
+++ src/com/company/UnsignedConverter.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class UnsignedConverter {
+    String byteOrder;
+    int sizeOfDataType;
+    String value;
+    void UnsignedConverter(String byteOrdering,int sizeOfDataType,String value){
+        this.byteOrder=byteOrdering;
+        this.sizeOfDataType=sizeOfDataType;
+        this.value=value;
+    }
+}
