f87aa80176ea999baa66afcdc1a9071b6ded1214
mrtzcn08 <mrtzcn08@gmail.com>
2022-03-28 12:01:59 +0300

    msg

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,46 @@
 package com.company;
-
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("hoşgelmişseniz");
-    }
+	public static void main(String[] args) {
+		
+		ArrayList<String> list = new ArrayList<String>();
+		int index = 0;
+		try {   
+			FileInputStream fis = new FileInputStream("input.txt");       
+			Scanner input = new Scanner(fis); 
+			while(input.hasNextLine()) {  
+				list.add(input.nextLine());
+			}  
+			input.close();     
+		}  
+		catch(IOException e) {  
+			e.printStackTrace();  
+		}  
+		for(int j = 0; j < list.size(); j++) {
+			read(list.get(j));
+		}
+		
+	}
+	static void read(String str) {
+		if(str.contains("u")) {
+			System.out.println("unsigned " + str);
+		}
+		else if(str.contains("-")) {
+			System.out.println("signed " + str);
+		}
+		else if(str.contains(".")) {
+			System.out.println("floating " + str);
+		}
+		else {
+			System.out.println("unsigned " + str);
+		}
+		
+	}
+	
+	
 }
+
+
